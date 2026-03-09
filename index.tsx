38db230878e2535a0225bc67625de01b68863b40
tony <9378129+casterfile@users.noreply.github.com>
2026-03-09 12:36:16 +0800

    Add version log v1.0.1 to console

--- index.tsx
+++ index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log('%cSpeechGyms v1.0.1', 'color: #6366f1; font-weight: bold; font-size: 14px;');
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
